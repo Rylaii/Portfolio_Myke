4155ae6d6228e6647587262221d37206e2c5b34c
mykebognalon <mtbognalon@kmbi.org.ph>
2026-02-07 21:10:34 +0800

    comment unused imports

--- src/common/SweetAlert.tsx
+++ src/common/SweetAlert.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import Swal from "sweetalert2";
+// import React from "react";
+// import Swal from "sweetalert2";
 
-export default function SweetAlert() {
-  const showAlert = () => {
-    Swal.fire({
-      position: "top-end",
-      icon: "success",
-      title: "Your work has been saved",
-      showConfirmButton: false,
-      timer: 1500,
-    });
-  };
+// export default function SweetAlert() {
+//   const showAlert = () => {
+//     Swal.fire({
+//       position: "top-end",
+//       icon: "success",
+//       title: "Your work has been saved",
+//       showConfirmButton: false,
+//       timer: 1500,
+//     });
+//   };
 
-  return (
-    <div>
-      <button onClick={showAlert}>Show Alert</button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <button onClick={showAlert}>Show Alert</button>
+//     </div>
+//   );
+// }
